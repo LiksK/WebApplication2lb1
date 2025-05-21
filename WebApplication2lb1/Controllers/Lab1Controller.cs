3ca9267fdc5dfd98f875a9281e53b80e6052e5fb
LiksL <LiksL@DESKTOP-G2QNRIL>
2025-05-21 18:06:34 +0300

    Add project files.

---
+++ WebApplication2lb1/Controllers/Lab1Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2lb1.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        // GET: Lab1
+        public ActionResult FirstViewMethod()
+        {
+            ViewBag.Vegetables = GetVegetablesList();
+            return View();
+        }
+        public ActionResult SecondViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+        public ActionResult ThirdViewMethod()
+        {
+            return View(GetVegetablesList().OrderBy(x => x).ToList());
+        }
+        public List<string> GetVegetablesList() { 
+            List<string> vegetables = new List<string>();
+            vegetables.Add("Tomatos");
+            vegetables.Add("Kartofels");
+            vegetables.Add("Baklazanes");
+            return vegetables;
+        }
+
+    }
+}
---
+++ WebApplication2lb1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication2lb1.MvcApplication" Language="C#" %>
